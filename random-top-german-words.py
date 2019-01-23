7107cd98ee380b58609e4845360b560515ef51a9
Mahmoud Adly <mahmoud.adly@gmail.com>
2019-01-23 07:19:09 +0100

    Add top 5k and 10k words

--- random-top-german-words.py
+++ random-top-german-words.py
@@ -3,17 +3,23 @@
 import random
 
 print("""Please choose which word list to use:
-1- Top 2000 words
-2- Top 25k words
-3- Top 50k words
+1- Top 2000 words (Wikipedia)
+2- Top 5k words (Internet corpus)
+3- Top 10k words (uni-leipzig.de)
+4- Top 25k words (OpenSubtitles 2016)
+5- Top 50k words (Internet corpus)
 """)
 
 c = raw_input()
 if c == "1":
     file_name = "./docs/top-2000.txt"
 elif c == "2":
-    file_name = "./docs/top-25k.txt"
+    file_name = "./docs/top-5k.txt"
 elif c == "3":
+    file_name = "./docs/top-10k.txt"
+elif c == "4":
+    file_name = "./docs/top-25k.txt"
+elif c == "5":
     file_name = "./docs/top-50k.txt"
 
 raw_data = open(file_name, 'r').read().decode("utf-8")
